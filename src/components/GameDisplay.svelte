b729f296ce7812a91a8ac0ad37d083f53a355c97
allanbraun <allanvobraun@gmail.com>
2021-06-06 15:32:33 -0300

    Layout game list

---
+++ src/components/GameDisplay.svelte
@@ -0,0 +1,88 @@
+<div class="game-card">
+
+    <img src="src/assets/images/portal2.jpg" alt="portal">
+    <div class="card-body">
+        <div class="card-center">
+            <span class="game-name">Portal 2</span>
+            <span class="game-date">
+                Added in 06/06/2021
+            </span>
+        </div>
+        <div class="card-end">
+            <button>
+                <Fa icon={faEdit} size="2x" class="svg"/>
+            </button>
+            <button>
+                <Fa icon={faTrashAlt} size="2x"/>
+            </button>
+        </div>
+    </div>
+</div>
+
+<script lang="ts">
+    import Fa from 'svelte-fa';
+    import {faEdit, faTrashAlt} from '@fortawesome/free-regular-svg-icons';
+</script>
+
+<style lang="scss">
+  @import "src/assets/scss/variables.scss";
+
+  button {
+    color: $orange !important;
+    background: none;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+  }
+
+  .game-card {
+    display: flex;
+    flex-direction: row;
+    margin: .5rem;
+    background-color: #333333;
+
+    img {
+      max-width: 40%;
+    }
+
+    .card-body {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+
+      .card-center {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 10px 14px;
+        flex-grow: 1;
+
+        .game-name {
+          font-size: 32px;
+          font-weight: 700;
+          line-height: 32px;
+          font-family: "Abel", sans-serif;
+          color: white;
+          align-self: flex-start;
+        }
+
+        .game-date {
+          line-height: 100%;
+          font-size: 18px;
+          align-self: flex-start;
+
+        }
+      }
+
+      .card-end {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 1rem 14px;
+      }
+    }
+  }
+
+</style>
---
+++ src/pages/Games.svelte
@@ -0,0 +1,83 @@
+<div class="main-wrapper">
+    <div class="content-wrapper">
+        <h1>Your game overs:</h1>
+        <div class="game-list">
+            <GameDisplay/>
+            <GameDisplay/>
+        </div>
+    </div>
+</div>
+
+<script lang="ts">
+    import GameDisplay from '@/components/GameDisplay.svelte';
+</script>
+
+<style lang="scss">
+
+  h1 {
+    font-family: "Abel", sans-serif;
+    font-size: 40px;
+    font-weight: 600;
+    line-height: 40px;
+    margin-block: 0;
+    text-decoration: none;
+    text-transform: none;
+    align-self: flex-start;
+    margin: 10px 0;
+  }
+  .main-wrapper {
+    align-items: center;
+    background-color: transparent;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+    z-index: 1;
+  }
+
+  .content-wrapper {
+    color: white;
+    font-size: 18px;
+    -moz-box-pack: center;
+    width: 100%;
+    min-width: 980px;
+    max-width: max-content;
+    padding: 30px 0;
+
+    @media only screen and (max-width: 1200px) {
+      min-width: 640px;
+      max-width: 910px;
+    }
+
+    @media only screen and (max-width: 960px) {
+      min-width: 600px;
+      max-width: 740px;
+    }
+
+    @media only screen and (max-width: 768px) {
+      width: 100%;
+      min-width: 320px;
+      max-width: 540px;
+    }
+
+    @media only screen and (max-width: 480px) {
+      min-width: unset;
+      padding: 56px 0;
+    }
+
+  }
+
+  .game-list {
+    display: flex;
+    flex-direction: column;
+    -moz-box-pack: center;
+    justify-content: center;
+    margin-top: unset;
+    width: 100%;
+    background-color: rgb(32, 35, 43);
+    border-bottom: 8px solid rgb(34, 34, 40);
+    flex-wrap: unset;
+    padding: .4rem;
+  }
+</style>
